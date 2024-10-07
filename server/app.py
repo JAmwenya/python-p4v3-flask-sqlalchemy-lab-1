17ab2702d90b023b0636a929ff24ece071a903bd
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-10-07 22:37:00 +0300

    creating views

--- server/app.py
+++ server/app.py
@@ -1,4 +1,3 @@
-# server/app.py
 #!/usr/bin/env python3
 
 from flask import Flask, make_response
@@ -21,7 +20,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>', methods=['GET'])
+def get_earthquake(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return make_response(earthquake.to_dict(), 200)
+    return make_response({'message': 'Earthquake 9999 not found.'}, 404)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>', methods=['GET'])
+def get_earthquake_magnitude(magnitude):
+    quakes = db.session.query(Earthquake).filter(Earthquake.magnitude >= magnitude).all()
+    quakes_list = [quake.to_dict() for quake in quakes]
+    if quakes:
+        return make_response(
+        {
+            'count': len(quakes),
+            'quakes': quakes_list
+        }, 
+        200
+    )
+
+    return make_response({'count': 0, 'quakes': []}, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
